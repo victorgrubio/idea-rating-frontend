d9d00c0e818e897a1abba6f382a92ac6f244da33
Victor Garcia Rubio <victorgrubiodl@gmail.com>
2021-09-08 10:27:51 +0200

    removed env in environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: process.env.API_URL || 'http://my-env.eba-g2gdst4z.us-west-2.elasticbeanstalk.com'
+  apiUrl: 'http://my-env.eba-g2gdst4z.us-west-2.elasticbeanstalk.com'
 };
