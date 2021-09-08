38bac4b98432b41e3257350e63ae12f3f9b4a765
Victor Garcia Rubio <victorgrubiodl@gmail.com>
2021-09-08 10:22:06 +0200

    Updated api url to beanstak

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://idea-rating-api.herokuapp.com'
+  apiUrl: process.env.API_URL || 'http://my-env.eba-g2gdst4z.us-west-2.elasticbeanstalk.com/'
 };
